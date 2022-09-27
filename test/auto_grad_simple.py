ab79e339b4a49abf6b813f450e54397538901504
mineself2016 <mineself2016@qq.com>
2022-09-26 23:41:35 -0400

    update the auto grad

--- test/auto_grad_simple.py
+++ test/auto_grad_simple.py
@@ -1,4 +1,5 @@
 #%%
+from tkinter import Y
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
@@ -47,10 +48,12 @@ def match_loss(gw_o, gw_s, device = "cpu"):
     for i in range(len(gw_o)):
         vec_o.append(gw_o[i].reshape(-1))
         vec_s.append(gw_s[i].reshape(-1))
-    vec_o = torch.cat(vec_o, dim = 0)
-    vec_s = torch.cat(vec_s, dim = 0)
 
-    dist = torch.sum((vec_o - vec_s) ** 2)
+        dist += torch.sum(torch.pow((gw_o[i] - gw_s[i]), 2))
+    # vec_o = torch.cat(vec_o, dim = 0)
+    # vec_s = torch.cat(vec_s, dim = 0)
+
+    # dist = torch.sum((vec_o - vec_s) ** 2)
 
     return dist
 
@@ -82,10 +85,10 @@ class MLP(nn.Module):
 model = MLP(num_o_features, num_h_features, num_o_classes)
 
 #%%
-def test_accuracy(model, labels: torch.Tensor):
+def test_accuracy(model, input, labels: torch.Tensor):
     
     with torch.no_grad():
-        output = model(x_o)
+        output = model(input)
 
     predicted = torch.argmax(output, dim = 1)
     predicted = predicted.numpy()
@@ -93,45 +96,74 @@ def test_accuracy(model, labels: torch.Tensor):
 
     acc = accuracy_score(labels, predicted)
 
-    print("acc: ", acc)
+    # print("acc: ", acc)
 
-    cm = confusion_matrix(labels, predicted)
-    print(cm)
+    # cm = confusion_matrix(labels, predicted)
+    # print(cm)
+    return acc
 
-    
+#%%
+def train():
+
+
+
+
+    pass
+
 #%%
 loss_fn = nn.CrossEntropyLoss()
-optim_model = torch.optim.SGD(model.parameters(), lr = 0.1)
+optim_model = torch.optim.SGD(model.parameters(), lr = 0.01)
 optim_feature = torch.optim.SGD([x_s], lr = 0.01)
 
 #%%
-model_parameters = list(model.parameters())
+outer_loop = 100
+inner_loop = 5
 
 #%%
-output_o = model(x_o)
-loss_o = loss_fn(output_o, y_o)
+loss_grad_list = []
+loss_class_list = []
+for i in range(outer_loop):
 
-gw_o = torch.autograd.grad(loss_o, model_parameters, create_graph = True)
+    model_parameters = list(model.parameters())
+
+    output_o = model(x_o)
+    loss_o = loss_fn(output_o, y_o)
+    gw_o = torch.autograd.grad(loss_o, model_parameters)
+
+    output_s = model(x_s)
+    loss_s = loss_fn(output_s, y_s)
+    gw_s = torch.autograd.grad(loss_s, model_parameters, create_graph = True)
+
+    # Update dataset
+    loss_grad = match_loss(gw_o, gw_s)
+
+    print(i, "th outer loop, loss", loss_grad.item())
+    loss_grad_list.append(loss_grad.item())
+
+    optim_feature.zero_grad()
+    loss_grad.backward()
+    optim_feature.step()
+
+    # Update model
+    for j in range(inner_loop):
+        x_s_inner = x_s.detach()
+        output_s_inner = model(x_s_inner)
+        loss_s_inner = loss_fn(output_s_inner, y_s)
+
+        if j == inner_loop - 1:
+            print("\tclassification loss:", loss_s_inner.item())
+            loss_class_list.append(loss_s_inner.item())
+            print("accuracy on origin data: ", test_accuracy(model, x_o, y_o))
+            print("accuracy on synthetic data: ", test_accuracy(model, x_s, y_s))
+
+        optim_model.zero_grad()
+        loss_s_inner.backward()
+        optim_model.step()
 
 #%%
-output_s = model(x_s)
-loss_s = loss_fn(output_s, y_s)
-
-gw_s = torch.autograd.grad(loss_s, model_parameters, create_graph = True)
-
-#%%
-loss = match_loss(gw_o, gw_s)
-
-print(loss.item())
-
-optim_feature.zero_grad()
-loss.backward()
-
-optim_feature.step()
-
-
-
-
+plt.plot(loss_grad_list, label = "grad")
+plt.plot(loss_class_list, label = "class")
+plt.legend()
 
 #%%
 for i, layer in enumerate(gw_o):
@@ -145,44 +177,3 @@ for i, layer in enumerate(gw_o):
     print("shape: ", layer.shape)
     print()
 
-
-#%%
-
-
-
-
-#%%
-"""
-loss_o = loss_fn(model(x_o), y_o)
-
-
-loss_o.backward()
-gw_o = model.parameters().grad.data
-
-loss_s = loss_fn(model(x_s), y_s)
-loss_s.backward()
-gw_s = model.parameters().grad.data
-
-
-match_loss:
-loss_grad = 0
-
-loss_grad = (gw_o - gw_s) ** 2
-
-
-
-"""
-
-# %%
-# for i in range(2):
-#     optim.zero_grad()
-#     output = model(x_o)
-
-#     loss = loss_fn(output, y_o)
-
-#     if i % 200 == 0:
-#         print("loss: ", loss.item())
-#         test_accuracy(model, y_o)
-
-#     loss.backward()
-#     optim.step()
